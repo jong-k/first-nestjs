624eb8e4f6201232c82759b806c2c89bcb5bba3a
jong-k <fully.charged07@gmail.com>
2023-11-20 23:50:09 +0900

    feat: User Repository 연결

--- mycar-value/src/users/users.service.ts
+++ mycar-value/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // private 키워드를 사용하여 프로퍼티 할당을 생략 가능
+  // InjectRepository 를 활용하여 의존성 주입
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    // typeorm의 save API: 주어진 엔터티를 데이터베이스에 저장
+    // 엔터티가 DB에 없으면 삽입, 있으면 업데이트
+    return this.repo.save(user);
+  }
+}
