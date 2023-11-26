03de68c7d87a0de3665fad98883a48cbf6c39efe
jong-k <fully.charged07@gmail.com>
2023-11-27 00:03:38 +0900

    docs: hash 관련 주석 추가

--- mycar-value/src/users/auth.service.ts
+++ mycar-value/src/users/auth.service.ts
@@ -14,6 +14,7 @@ export class AuthService {
     if (users.length) throw new BadRequestException("email in use");
     const salt = randomBytes(8).toString("hex"); // 8byte의 2진수를 16진수 문자열로 변환
     const hash = (await scrypt(password, salt, 32)) as Buffer; // password와 salt를 합쳐 32자의 문자 hash 반환
+    // 결과 3f1415e012475090.de22ce3fabf18eb1fd79de1f6691520f368c7df39c5dce3184702b762a5ec2cb
     const result = salt + "." + hash.toString("hex");
     const user = await this.usersService.create(email, result);
     return user;
