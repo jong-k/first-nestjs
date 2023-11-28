8bcb8ba41537f38227cb0036a28a1e47644f5a68
jong-k <fully.charged07@gmail.com>
2023-11-29 07:57:37 +0900

    feat: current user 확인 인터셉터 추가

--- mycar-value/src/users/decorators/current-user.decorator.ts
+++ mycar-value/src/users/decorators/current-user.decorator.ts
@@ -1,10 +1,9 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-// data가 절대 쓰이지 않을 것이라면 never
 export const CurrentUser = createParamDecorator(
+  // data가 절대 쓰이지 않을 것이라면 never
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    console.log(request.session.userId);
-    return;
+    return request.currentUser;
   },
 );
--- mycar-value/src/users/users.controller.ts
+++ mycar-value/src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   NotFoundException,
   Session,
+  UseInterceptors,
 } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
@@ -17,9 +18,12 @@ import { Serialize } from "../interceptors/serialize.interceptor";
 import { UserDto } from "./dtos/user.dto";
 import { AuthService } from "./auth.service";
 import { CurrentUser } from "./decorators/current-user.decorator";
+import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
+import { User } from "./user.entity";
 
 @Controller("auth")
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
@@ -44,7 +48,7 @@ export class UsersController {
   // }
 
   @Get("whoami")
-  whoAmI(@CurrentUser() user: null) {
+  whoAmI(@CurrentUser() user: User) {
     return user;
   }
 
