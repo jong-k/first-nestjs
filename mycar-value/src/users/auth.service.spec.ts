9a2142eb6cd41297c22b7b220eb6555f41951ddd
jong-k <fully.charged07@gmail.com>
2023-12-01 07:17:36 +0900

    refactor: auth service test 개선

--- mycar-value/src/users/auth.service.spec.ts
+++ mycar-value/src/users/auth.service.spec.ts
@@ -1,23 +1,33 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
+import { User } from "./user.entity";
 
-it("can create an instance of auth service", async () => {
-  // auth service에 user service 의존성 주입을 위해 만든 fake class
-  const faksUsersService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  };
+// test 제목
+describe("AuthService", () => {
+  let service: AuthService;
 
-  const module = await Test.createTestingModule({
-    // testing DI 컨테이너에 등록할 것들
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: faksUsersService },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // auth service에 user service 의존성 주입을 위해 만든 fake class
+    const faksUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+    const module = await Test.createTestingModule({
+      // testing DI 컨테이너에 등록할 것들
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: faksUsersService },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  // test 소제목
+  it("can create an instance of auth service", async () => {
+    expect(service).toBeDefined();
+  });
 });
